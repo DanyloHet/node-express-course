3fdd48594d08234c0f2e3113b258a68873f2ba5a
Daniel <150855751+DanyloHet@users.noreply.github.com>
2025-08-22 18:48:21 +0300

    Update 08-os-module.js

--- 01-node-tutorial/answers/08-os-module.js
+++ 01-node-tutorial/answers/08-os-module.js
@@ -7,7 +7,7 @@ console.log(user);
 //method returns the system uptime in seconds
 console.log(`The system uptime is ${os.uptime()} seconds`);
 
-const curresntOS = {
+const currentOS = {
     name: os.type(),
     release: os.release(),
     totalMem: os.totalmem(),
